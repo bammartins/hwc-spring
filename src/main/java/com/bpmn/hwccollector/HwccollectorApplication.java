b0b4721eb8010553bbba5b9b8275a94105c9de10
= <=>
2024-01-30 20:03:38 -0300

    First Commit

---
+++ src/main/java/com/bpmn/hwccollector/HwccollectorApplication.java
@@ -0,0 +1,13 @@
+package com.bpmn.hwccollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwccollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwccollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bpmn/hwccollector/HwccollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpmn.hwccollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwccollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
